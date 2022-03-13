bb39824bc614eb5e7de59e8ecb8df2cc75e22dba
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-03-13 15:55:41 +0300

    Optimizing Client/Server Development Workflow

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,12 @@ function App() {
     const urlParams = new URLSearchParams(queryString);
     const accessToken = urlParams.get("access_token");
     const refreshToken = urlParams.get("refresh_token");
-    console.log(accessToken, refreshToken);
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    }
   });
 
   return (
