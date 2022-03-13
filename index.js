e29aecaf74a905467e9d888e3de522ec583e8b13
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-03-13 15:26:24 +0300

    Create React App & Passing Tokens

--- index.js
+++ index.js
@@ -75,21 +75,14 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
+        const { access_token, refresh_token } = response.data;
 
-        //Use the Access Token
-        axios
-          .get("https://api.spotify.com/v1/me", {
-            headers: {
-              Authorization: `${token_type} ${access_token}`,
-            },
-          })
-          .then((response) => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch((error) => {
-            res.send(error);
-          });
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+        });
+        //Send this information to the client
+        res.redirect(`http://localhost:3000?${queryParams}`);
       } else {
         res.send(response);
       }
